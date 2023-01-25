d3cdb6792defa65c27be1564684b093ed2930129
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-01-26 00:48:29 +0300

    added usestate and useeffect

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-function HomePage() {
+  function HomePage() {
+    const [movies, setMovies] = useState([]);
+  
+    useEffect(() => {
+      fetch(
+        "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e3a92895b0a6c5293e775e40825cf6cf"
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setMovies(data.results);
+          console.log(data);
+        });
+        
+    }, []);
   return (
     <div>HomePage</div>
   )
