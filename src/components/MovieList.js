8c9a2966a1764d9e85c2a861be7762644cf5006c
Paul <paul.ngugi@student.moringaschool.com>
2023-01-26 00:18:13 +0300

    added container

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MovieList({ movies }) {
   return (
-    <div >
+    <div className="container">
       <div className="row ">
         {movies.map((movie) => (
           <Card key={movie.id} movie={movie}/> 
