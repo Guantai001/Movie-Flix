a36530c5d473aa25408d4d9c897e05fb5ee26811
guantai <jpguantai@gmail.com>
2023-01-26 00:04:21 +0300

    created states

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,10 @@ import React from "react";
 
 function Card(){
     
+    const [email, setEmail] = useState("");
+    const [comment, setComment] = useState("");
+    const [rate, setRate] = useState("");
+
     return(
         <div>
             <h1>Card</h1>
