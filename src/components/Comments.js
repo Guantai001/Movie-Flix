2a983e90212c0dc634388c87f32f587d4cdb9312
Paul <paul.ngugi@student.moringaschool.com>
2023-01-26 00:35:09 +0300

    added states

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React,{useState} from "react";
 
 function Comments() {
+  const [getComments, setComments] = useState([]);
+
   return (
     <div>Comments</div>
   )
