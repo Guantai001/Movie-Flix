52f954b34007c2566168df2c470a64b6aaf54adf
Paul <paul.ngugi@student.moringaschool.com>
2023-01-26 01:00:04 +0300

    added a delete function

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -14,6 +14,18 @@ function Comments() {
     });
 }, []);
 
+function handleDelete(id) {
+  fetch(`http://localhost:4001/movies/${id}`, {
+      method: "DELETE",
+  })
+  .then((res) => res.json())
+  .then((data) => {
+      console.log(data);
+      
+      setComments(getComments.filter((comment) => comment.id !== id));
+  });
+}
+
   return (
     <div>
       <Search />
@@ -38,6 +50,7 @@ function Comments() {
                             <td>{comment.comment}</td>
                             <td>{comment.rate}</td>
                             <td><button 
+                            onClick={() => handleDelete(comment.id)}
                             className="btn btn-danger">Delete</button></td>
                         </tr>
                     ))}
