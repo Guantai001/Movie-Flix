4517f148d3487ed47f02b6df927a994f2f9f1d7e
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-01-26 01:47:11 +0300

    added search function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
-function Search() {
+  function Search({ handleSearch , handleFilter}) {
+    const BASE_URL = "https://api.themoviedb.org/3";
+    const API_KEY = "api_key=e3a92895b0a6c5293e775e40825cf6cf";
+    const SEARCH_URL = `${BASE_URL}/search/movie?${API_KEY}`;
   return (
     <div>Search</div>
   )
