c4645dfbad4f9631a9cdcb7da59f1c969cbf915e
Paul <paul.ngugi@student.moringaschool.com>
2023-01-26 00:38:26 +0300

    added useeffect

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,7 +1,16 @@
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 
 function Comments() {
   const [getComments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4001/movies")
+    .then((res) => res.json())
+    .then((data) => {
+        setComments(data);
+        console.log(data);
+    });
+}, []);
 
   return (
     <div>Comments</div>
